51a988c49eaf3c66d2951527f6eee662effdf916
Ivan Stanishev <istanishev@cimpress.com>
2019-02-12 13:30:48 +0100

    Rearragne

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import {blockTypes} from './blockTypes';
+import * as uuidv1 from 'uuid/v1';
 import {mainToMjml} from './blocks/main';
 import {textToMjml} from './blocks/text';
 import {rowToMjml} from './blocks/row';
 import {columnToMjml} from './blocks/column';
 import {buttonToMjml} from './blocks/button';
 import {loopToMjml} from './blocks/loop';
+
+const EDIE_BLOCK_TYPE = {
+    MAIN: 'main',
+    COLUMN: 'column',
+    ROW: 'row',
+    TEXT: 'text',
+    BUTTON: 'button',
+    LOOP: 'loop',
+};
 
 const blockToMjml = (item, childrenRenderer) => {
     switch (item.type) {
@@ -41,20 +50,60 @@ function edie2hbsmjml(edieJson) {
     return `<mjml><mj-head>${exportMimeHeadersToMjml(edieJson.mimeHeaders)}</mj-head>${blockToMjml(edieJson.structure, blockToMjml)}</mjml>`;
 }
 
-const emptyStructure = {
-    formatVersion: 'v1.0',
-    mimeHeaders: {
-        to: 'a@b.com',
-        subject: 'sample email',
-    },
-    structure: {
-        id: 'main',
-        type: 'main',
-        children: [],
-    },
-};
+const createEmptyFormat = (v) => {
+    if (v && v !== 'v1.0') {
+        return null
+    }
+
+    return {
+        formatVersion: v || 'v1.0',
+        mimeHeaders: {
+            to: 'a@b.com',
+            subject: 'sample email',
+        },
+        structure: {
+            id: 'main',
+            type: 'main',
+            children: [],
+        },
+    };
+}
+
+
+function createEmptyBlock(type) {
+    let props = {};
+    switch (type) {
+    case EDIE_BLOCK_TYPE.BUTTON:
+        props = {
+            backgroundColor: '#188ec5',
+            color: '#ffffff',
+            textAlign: 'center',
+            borderSize: '0px',
+            borderColor: 'transparent',
+        };
+        break;
+
+    case EDIE_BLOCK_TYPE.COLUMN:
+        props = {
+            width: '100%',
+        };
+        break;
+
+    default:
+        break;
+    }
+
+    return {
+        id: uuidv1(),
+        type: type,
+        properties: props,
+        children: [EDIE_BLOCK_TYPE.ROW, EDIE_BLOCK_TYPE.COLUMN, EDIE_BLOCK_TYPE.LOOP].includes(type) ? [] : null,
+    };
+}
 
 export {
+    EDIE_BLOCK_TYPE,
     edie2hbsmjml,
-    emptyStructure,
+    createEmptyBlock,
+    createEmptyFormat,
 };
